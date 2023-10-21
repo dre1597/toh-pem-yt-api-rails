cebca6c48bf8fd89528b8912f8bc6db14f4fd441
André Pereira <andrepereira97@yahoo.com>
2023-10-20 23:49:34 -0300

    :white_check_mark: add heroes sorted by name test

--- spec/models/hero_spec.rb
+++ spec/models/hero_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Hero, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
   end
+
+  describe "scopes" do
+    it "returns all heroes sorted by name" do
+      expect(Hero.sorted_by_name.to_sql).to eq Hero.order(:name).to_sql
+    end
+  end
 end
